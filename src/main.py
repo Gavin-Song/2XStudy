3750292edf9c71a7e50c10fd22edbe1ff9c9b458
omarASC5 <ocobas19@avenues.org>
2019-11-02 16:02:05 -0400

    script complete

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pytube
 import os
 import datetime
+import json
 
 video_url = 'https://www.youtube.com/watch?v=s4gBChg6AII'
 
@@ -17,21 +18,15 @@ video_author = video_data['author']
 video_length_in_seconds = video_data['lengthSeconds']
 video_length_formatted = str(datetime.timedelta(seconds=int(video_length_in_seconds)))
 
-print(video_id)
-print(video_title)
-print(video_author)
-print(video_length_in_seconds)
-print(video_length_formatted)
-
 newpath = './saves/' + video_id
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 
 video.download('./saves/' + video_id, 'temp')
 
-meta_data = []
+meta_data = {'video_id': video_id, 'video_title': video_title, \
+             'video_author': video_author, 'video_length': video_length_formatted}
 
-output_file = 'meta_data.txt'
-with open(output_file, 'w+') as f:
-  for line in meta_data:
-    f.write(line)
+output_file = 'meta_data.json'
+with open('./saves/' + video_id + '/' + output_file, 'w+') as f:
+  json.dump(meta_data, f)
